3831ae0487d7e5b30f958ae5fe558d4a3d63c30b
Kamil Rominski <kamil.rominski@gmail.com>
2019-02-24 12:00:51 +0100

    Change in classes
    Completed transfer to classes and fixed most of the bugs

--- main.py
+++ main.py
@@ -1,92 +1,131 @@
-import math
 import random
 import os
 
-def main():
-	while True:
-		os.system("cls")
-		game=input("Do you want to play hangman? Y=yes N=no: ")
-		if game == "y":
-			break
-		elif game == "n":
-			print("Bye bye :)")
-			exit()
-	while(game=="y"):
-		#reading words from file
-		words=open("words.txt","r")
-		bank=["word"]
-		if words.mode == "r":
-			contents = words.read().splitlines()
-		attempts=11 #5 to stand and 6 as human
-		#randomizing word from array and creating word template to show to player
-		rnd=random.randint(0,9897) #9898 words
-		word=contents[rnd]
-		wordlen=len(word)
-		guess=[]
-		letters=[]
-		words=[]
-		for character in word:
+menu = ""
+attempts = 11
+attempt = ""
+guessrpl = 0
+word = ""
+wordlen = 0
+
+def file():
+	global wordlen
+	global word
+
+	words=open("words.txt","r")
+	bank=["word"]
+	if words.mode == "r":
+		contents = words.read().splitlines()
+	rnd=random.randint(0,9897) #9898 words
+	word=contents[rnd]
+	wordlen=len(word)
+
+def game():
+	global attempts
+	global attempt
+	global guessrpl
+	global word
+	global wordlen
+	
+	attempts = 11
+	guessrpl = 0
+	guess=[]
+	letters=[]
+	words=[]
+	file()
+	for character in word:
 			guess.append("-")
-		guessrpl=0 #0 for first round, after that 1-correct, 2-incorrect
-		attempt="" #variable with input character
-		while attempts>0:
-			while True:
-				os.system('cls')
-				if guessrpl == 1:
-					print("You guessed correctly!\n")
-				elif guessrpl == 2:
-					print("You didn't guess right :< Try again!\n")
-				elif guessrpl == 3:
-					print('You choose already this letter! Try another one.\n')
-				elif guessrpl == 4:
-					print('You choose already this word! Try another one.\n')
-				print("Guess the word! You have",attempts,"attempts left\n")
-				print("\nLetters already used: ", letters,"\n")
-				print("\nWords tried: ",words,"\n")
-				print("Word to guess:")
-				print(guess)
-				attempt=input("\nYour type: ")
-				if len(attempt)<2:
-					if attempt not in letters:
-						letters.append(attempt)
-						break
-					else:
-						guessrpl = 3
-				else:
-					if attempt not in words:
-						words.append(attempt)
-						break
-					else:
-						guessrpl = 4
-			#checking player guess
-			if len(attempt)>1:
-				if attempt == word:
+	while attempts>0:
+		while True:
+			os.system('cls')
+			if guessrpl == 1:
+				print("You guessed correctly!\n")
+			elif guessrpl == 2:
+				print("You didn't guess right :< Try again!\n")
+			elif guessrpl == 3:
+				print('You already tried this letter! Try another one.\n')
+			elif guessrpl == 4:
+				print('You already tried this word! Try another one.\n')
+			print("Guess the word! You have",attempts,"attempts left\n")
+			print("\nLetters already used: ", letters,"\n")
+			print("\nWords tried: ",words,"\n")
+			print("Word to guess:")
+			print(guess)
+			print(word)
+			attempt=input("\nYour type: ")
+			if len(attempt)<2:
+				if attempt not in letters:
+					letters.append(attempt)
 					break
 				else:
-					attempts=attempts-1
-					guessrpl = 2
-			elif attempt in word:
-				
-				guessrpl=1
-				for x in range (0, wordlen):
-					if word[x]==attempt:
-						guess[x]=attempt #insert guess letter to storage to print
-				if not '-' in guess:
-					break
+					guessrpl = 3
 			else:
-				guessrpl=2
+				if attempt not in words:
+					words.append(attempt)
+					break
+				else:
+					guessrpl = 4
+		if len(attempt)>1:
+			if attempt == word:
+				end()
+			else:
 				attempts=attempts-1
-		if attempts==0:
-			os.system('cls')
-			game=input("You lost! Do you want to try again? Y=yes N=no: ")
-			if(game!="y"):
-				print("Bye bye! :)")
-				break
+				guessrpl = 2
+		elif attempt in word:
+			
+			guessrpl=1
+			for x in range (0, wordlen):
+				if word[x]==attempt:
+					guess[x]=attempt #insert guess letter to storage to print
+			if not '-' in guess:
+				end()
 		else:
-			os.system('cls')
-			game=input("You won! Do you want to try again? Y=yes N=no: ")
-			if(game!="y"):
+			guessrpl=2
+			attempts=attempts-1
+	
+
+def end():
+	global menu
+	global attempts
+
+	if menu == "n":
+		print("Bye bye :)")
+		exit()
+		
+	if attempts < 1:
+		os.system('cls')
+		while True:
+			menu=input("You lost! Do you want to try again? Y=yes N=no: ")
+			if(menu == "n"):
 				print("Bye bye! :)")
-				break
-if __name__=="__main__":
-	main()
+				exit()
+			elif menu == "y":
+				attempts = 11
+				game()
+				
+	elif attempts>0:
+		os.system('cls')
+		while True:
+			menu = input("You won! Do you want to try again? Y=yes N=no: ")
+			if(menu == "n"):
+				print("Bye bye! :)")
+				exit()
+			elif menu == "y":
+				attempts = 11
+				game()
+				
+def start():
+	global menu
+	startvar = 1
+	
+	while startvar == 1:
+		os.system('cls')
+		menu = input('Do you want to play hangman? Y=yes N=no: ')
+		if menu == "y":
+			startvar = 0
+			game()
+		elif menu == "n":
+			end()
+			
+if __name__ == "__main__":
+	start()
